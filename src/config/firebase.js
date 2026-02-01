0bccc0326a62866f859a32de96b2fc722cc7a2d5
NattatidaBt <166736170+NattatidaBt@users.noreply.github.com>
2026-02-01 17:55:20 +0700

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage"; // <-- ต้องมี storage
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqvZWSIiS2GrCI0fZ5fNEVEdkA-SFpl-4",
-  authDomain: "recalpt-59d0f.firebaseapp.com",
-  projectId: "recalpt-59d0f",
-  storageBucket: "recalpt-59d0f.firebasestorage.app",
-  messagingSenderId: "721060586583",
-  appId: "1:721060586583:web:9c854dab45bcc3c2fbaf12",
-  measurementId: "G-F6SEH4L88Z",
+  
 };
 
 const app = initializeApp(firebaseConfig);
